b77ca6113cde914a077c90d6090f3340b422ef67
Ijaz Ali <ijazalideveloper@gmail.com>
2025-04-14 23:09:19 +0400

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
 export default function Home() {
   // Redirect to markets page
-  redirect('/markets');
-  
+  redirect("/markets");
+
   // This will never be rendered
   return null;
 }
